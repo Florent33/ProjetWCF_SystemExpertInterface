1c1a549aada3cad32814e0381a89d90dd867ec71
Florent33 <ferrand.florent@gmail.com>
2015-03-18 10:23:01 +0100

    commit

--- ProjetWebService_SystemeExpert/Program.cs
+++ ProjetWebService_SystemeExpert/Program.cs
@@ -16,7 +16,7 @@ namespace ProjetWebService_SystemeExpert
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
         }
     }
 }
